d83f0fb06aa753d7f7e6d9b43f4eaeede29f1d90
Oluwaseun <adunmofestus@gmail.com>
2023-09-30 22:45:53 +0100

    icon added to script

--- SCRIPT.js
+++ SCRIPT.js
@@ -9,5 +9,10 @@ function toggleBtn(){
 var icon = document.getElementById("icon");
 
 icon.onclick = function(){
-    document.body.classList.toggle("dark-theme")
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "dark theme icon/sun.png";
+    }else{
+        icon.src = "dark theme icon/moon.png";
+    }
 }
